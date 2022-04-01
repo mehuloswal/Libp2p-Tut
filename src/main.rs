0267af281c80dadf80e7dbfdf2d5ee64b035de36
Mehul Oswal <mehuloswal21@gmail.com>
2022-04-01 09:30:51 +0530

    Getting the local peer id

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 //! Nodes identify each other via their [`PeerId`](crate::PeerId) which is
 //! derived from their public key.
 use libp2p::{identity, PeerId};
-use std::error::Error
+use std::error::Error;
+
+#[async_std::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let local_key = identity::Keypair::generate_ed25519();
+    let local_peer_id = PeerId::from(local_key.public());
+    println!("Local peer id: {:?}", local_peer_id);
+    Ok(())
+}
